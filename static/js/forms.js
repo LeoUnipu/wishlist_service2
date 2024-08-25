23c09e6fa4342750d084adef97d79a2c47a09d85
LeoUnipu <158611789+LeoUnipu@users.noreply.github.com>
2024-08-26 00:20:50 +0200

    Add files via upload

---
+++ static/js/forms.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById('itemForm');
+    form.addEventListener('submit', function (event) {
+        const naziv = document.getElementById('naziv').value;
+        const cijena = document.getElementById('cijena').value;
+
+        if (!naziv || !cijena) {
+            alert("Sva polja moraju biti popunjena!");
+            event.preventDefault();  // Spriječi slanje forme
+        }
+    });
+});
